cc2f6206d163be32b4ac95947d1e54832416ba0f
Karoline Pauls <code@karolinepauls.com>
2021-12-10 18:24:53 +0000

    mandatory timeout

--- valueonly/context.go
+++ valueonly/context.go
@@ -6,8 +6,16 @@ import (
 )
 
 // Context that wraps another and suppresses its deadline or cancellation.
-type Context struct{ context.Context }
+type valueOnlyContext struct{ context.Context }
 
-func (Context) Deadline() (deadline time.Time, ok bool) { return }
-func (Context) Done() <-chan struct{}                   { return nil }
-func (Context) Err() error                              { return nil }
+func WithNewDeadline(parent context.Context, deadline time.Time) (context.Context, context.CancelFunc) {
+	return context.WithDeadline(&valueOnlyContext{parent}, deadline)
+}
+
+func WithNewTimeout(parent context.Context, timeout time.Duration) (context.Context, context.CancelFunc) {
+	return context.WithTimeout(&valueOnlyContext{parent}, timeout)
+}
+
+func (valueOnlyContext) Deadline() (deadline time.Time, ok bool) { return }
+func (valueOnlyContext) Done() <-chan struct{}                   { return nil }
+func (valueOnlyContext) Err() error                              { return nil }
--- valueonly/context_test.go
+++ valueonly/context_test.go
@@ -2,6 +2,7 @@ package valueonly
 
 import (
 	"context"
+	"fmt"
 	"testing"
 	"time"
 
@@ -10,18 +11,58 @@ import (
 
 func TestContextValues(t *testing.T) {
 	ctx := context.WithValue(context.Background(), "testkey", "testvalue")
-
-	derivedCtx := &Context{ctx}
+	derivedCtx, _ := WithNewDeadline(ctx, time.Now())
 	assert.Equal(t, derivedCtx.Value("testkey"), "testvalue")
 }
 
-func TestContextDeadline(t *testing.T) {
-	// Creating original context and deadlining it.
-	ctx := context.Background()
-	setDeadline := time.Now().Add(-time.Second)
-	ctx, cancel := context.WithDeadline(ctx, setDeadline)
-	defer cancel()
+func TestNewContextExpiration(t *testing.T) {
+	oldDeadline := time.Now().Add(-time.Minute)
+	oldCtx, _ := context.WithDeadline(context.Background(), oldDeadline)
 
+	t.Run("deadline", func(t *testing.T) {
+		newDeadline := time.Now().Add(time.Minute)
+		derivedCtx, _ := WithNewDeadline(oldCtx, newDeadline)
+
+		actualDeadline, _ := derivedCtx.Deadline()
+		assert.Equal(t, actualDeadline, newDeadline)
+		assert.Check(t, actualDeadline != oldDeadline)
+	})
+
+	t.Run("timeout", func(t *testing.T) {
+		now := time.Now()
+		timeout := time.Second * 100
+		derivedCtx, _ := WithNewTimeout(oldCtx, timeout)
+
+		actualDeadline, _ := derivedCtx.Deadline()
+		expectedDeadline := now.Add(timeout)
+		delta := actualDeadline.Sub(expectedDeadline)
+		assert.Check(t, delta < time.Millisecond,
+			fmt.Sprintf("real deadline: %v must be within 1ms since the expected deadline: %v, is %v",
+				actualDeadline, expectedDeadline, delta))
+		assert.Check(t, actualDeadline != oldDeadline)
+	})
+}
+
+func TestContextCancellation(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	derivedCtx, derivedCancel := WithNewTimeout(ctx, time.Second*10)
+
+	assert.Check(t, !isDone(ctx))
+	assert.Check(t, !isDone(derivedCtx))
+
+	cancel()
+
+	assert.Check(t, isDone(ctx))
+	assert.Check(t, !isDone(derivedCtx))
+
+	assert.Equal(t, ctx.Err().Error(), "context canceled", "original context cancelled")
+	assert.NilError(t, derivedCtx.Err(), "derived context not cancelled")
+
+	derivedCancel()
+	assert.Equal(t, derivedCtx.Err().Error(), "context canceled", "derived context cancelled")
+}
+
+func isDone(ctx context.Context) bool {
 	done := false
 	select {
 	case <-ctx.Done():
@@ -29,31 +70,48 @@ func TestContextDeadline(t *testing.T) {
 	default:
 		done = false
 	}
-	actualDeadline, deadlineSet := ctx.Deadline()
-	assert.Equal(t, actualDeadline, setDeadline, "original context's deadline is set")
-	assert.Equal(t, deadlineSet, true, "original context's deadline is set")
-	assert.Equal(t, done, true, "original context's done channel resolved")
-	assert.Equal(t, ctx.Err().Error(), "context deadline exceeded",
-		"original context cancelled by deadline")
-
-	// Checking that the derived context is not cancelled.
-	derivedCtx := &Context{ctx}
-	derivedDone := false
-	select {
-	case <-derivedCtx.Done():
-		derivedDone = true
-	default:
-		derivedDone = false
-	}
-
-	_, derivedDeadlineSet := derivedCtx.Deadline()
-	assert.Equal(t, derivedDeadlineSet, false, "derived context has no deadline set")
-	assert.Equal(t, derivedDone, false, "derived context's done channel not resolved")
-	assert.Equal(t, derivedCtx.Err(), nil, "derived context's error is nil")
-
-	// Checking that we can cancel the derived context.
-	derivedCtxTryCancel, cancel := context.WithCancel(derivedCtx)
-	cancel()
-	assert.Equal(t, derivedCtxTryCancel.Err().Error(), "context canceled",
-		"can cancel a derived context")
+	return done
 }
+
+//func TestContextCancellation(t *testing.T) {
+//	// Creating original context and deadlining it.
+//	ctx := context.Background()
+//	setDeadline := time.Now().Add(-time.Second)
+//	ctx, cancel := context.WithDeadline(ctx, setDeadline)
+//	defer cancel()
+//
+//	done := false
+//	select {
+//	case <-ctx.Done():
+//		done = true
+//	default:
+//		done = false
+//	}
+//	actualDeadline, deadlineSet := ctx.Deadline()
+//	assert.Equal(t, actualDeadline, setDeadline, "original context's deadline is set")
+//	assert.Equal(t, deadlineSet, true, "original context's deadline is set")
+//	assert.Equal(t, done, true, "original context's done channel resolved")
+//	assert.Equal(t, ctx.Err().Error(), "context deadline exceeded",
+//		"original context cancelled by deadline")
+//
+//	// Checking that the derived context is not cancelled.
+//	derivedCtx := &Context{ctx}
+//	derivedDone := false
+//	select {
+//	case <-derivedCtx.Done():
+//		derivedDone = true
+//	default:
+//		derivedDone = false
+//	}
+//
+//	_, derivedDeadlineSet := derivedCtx.Deadline()
+//	assert.Equal(t, derivedDeadlineSet, false, "derived context has no deadline set")
+//	assert.Equal(t, derivedDone, false, "derived context's done channel not resolved")
+//	assert.Equal(t, derivedCtx.Err(), nil, "derived context's error is nil")
+//
+//	// Checking that we can cancel the derived context.
+//	derivedCtxTryCancel, cancel := context.WithCancel(derivedCtx)
+//	cancel()
+//	assert.Equal(t, derivedCtxTryCancel.Err().Error(), "context canceled",
+//		"can cancel a derived context")
+//}
